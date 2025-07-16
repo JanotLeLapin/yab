713fc34b5ed7adaac03075f5b81beb7d5da2da76
Joseph DALY <joseph300905@gmail.com>
2025-07-16 16:46:49 +0200

    create a custom message

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,5 +1,6 @@
 use std::{env, error::Error};
 use twilight_gateway::{Event, EventTypeFlags, Intents, Shard, ShardId, StreamExt as _};
+use twilight_http::Client;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
@@ -8,7 +9,8 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
         | Intents::GUILD_MEMBERS
         | Intents::GUILD_MESSAGE_TYPING
         | Intents::MESSAGE_CONTENT;
-    let mut shard = Shard::new(ShardId::ONE, token, intents);
+    let mut shard = Shard::new(ShardId::ONE, token.clone(), intents);
+    let client = Client::new(token);
 
     while let Some(item) = shard.next_event(EventTypeFlags::all()).await {
         let Ok(event) = item else {
@@ -21,6 +23,14 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
                 if msg.author.bot {
                     continue;
                 }
+
+                client
+                    .create_message(msg.channel_id)
+                    .content(&format!(
+                        "hello, {}! I was build with Twilight.",
+                        msg.author.global_name.as_ref().unwrap_or(&msg.author.name)
+                    ))
+                    .await?;
             }
             _ => {
                 println!("{event:?}");
