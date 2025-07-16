7c5415ca54e4b665bfdbe7d04fa2056219dd3d08
Joseph DALY <joseph300905@gmail.com>
2025-07-16 17:20:58 +0200

    use tokio tracing

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -2,24 +2,31 @@ use std::{env, error::Error};
 use twilight_gateway::{Event, EventTypeFlags, Intents, Shard, ShardId, StreamExt as _};
 use twilight_http::Client;
 
+use tracing::{debug, info, warn};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
     let token = env::var("DISCORD_TOKEN")?;
     let intents = Intents::GUILD_MESSAGES
         | Intents::GUILD_MEMBERS
         | Intents::GUILD_MESSAGE_TYPING
         | Intents::MESSAGE_CONTENT;
     let mut shard = Shard::new(ShardId::ONE, token.clone(), intents);
+    info!("created shard");
+
     let client = Client::new(token);
 
     while let Some(item) = shard.next_event(EventTypeFlags::all()).await {
         let Ok(event) = item else {
-            println!("could not receive event");
+            warn!(source = ?item.unwrap_err(), "error receiving event");
             continue;
         };
         match event {
             Event::MessageCreate(msg) => {
-                println!("{}: {}", msg.author.name, msg.content);
                 if msg.author.bot {
                     continue;
                 }
@@ -33,7 +40,7 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
                     .await?;
             }
             _ => {
-                println!("{event:?}");
+                debug!(?event, "event");
             }
         }
     }
