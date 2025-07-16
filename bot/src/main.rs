0e4cf226e2ea0827fd58f0a0b082769633a897ed
Joseph DALY <joseph300905@gmail.com>
2025-07-16 16:22:46 +0200

    message logger test

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,10 +1,13 @@
 use std::{env, error::Error};
-use twilight_gateway::{EventTypeFlags, Intents, Shard, ShardId, StreamExt as _};
+use twilight_gateway::{Event, EventTypeFlags, Intents, Shard, ShardId, StreamExt as _};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
     let token = env::var("DISCORD_TOKEN")?;
-    let intents = Intents::GUILD_MESSAGES;
+    let intents = Intents::GUILD_MESSAGES
+        | Intents::GUILD_MEMBERS
+        | Intents::GUILD_MESSAGE_TYPING
+        | Intents::MESSAGE_CONTENT;
     let mut shard = Shard::new(ShardId::ONE, token, intents);
 
     while let Some(item) = shard.next_event(EventTypeFlags::all()).await {
@@ -12,7 +15,17 @@ async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
             println!("could not receive event");
             continue;
         };
-        println!("{event:?}");
+        match event {
+            Event::MessageCreate(msg) => {
+                println!("{}: {}", msg.author.name, msg.content);
+                if msg.author.bot {
+                    continue;
+                }
+            }
+            _ => {
+                println!("{event:?}");
+            }
+        }
     }
 
     Ok(())
